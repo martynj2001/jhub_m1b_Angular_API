0ff94213998fe0764b46f0af6e25f9177a5f0734
martynj2001 <martynj2010@icloud.com>
2022-06-06 13:25:44 +0100

    Generated Componants

--- Data_API/src/app/app.module.ts
+++ Data_API/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FilterComponent } from './filter/filter.component';
+import { DataSelectComponent } from './data-select/data-select.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilterComponent,
+    DataSelectComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
---
+++ Data_API/src/app/data-select/data-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-select',
+  templateUrl: './data-select.component.html',
+  styleUrls: ['./data-select.component.css']
+})
+export class DataSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
